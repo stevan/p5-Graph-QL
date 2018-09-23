6b173c8e6e490c90beb435093c531c5412d40a15
Stevan Little <stevan.little@gmail.com>
2018-09-23 19:13:32 +0200

    simple execution is working!

--- lib/Graph/QL/Execution/ExecuteQuery.pm
+++ lib/Graph/QL/Execution/ExecuteQuery.pm
@@ -16,19 +16,17 @@ our $VERSION = '0.01';
 
 use parent 'UNIVERSAL::Object::Immutable';
 use slots (
-    name      => sub {}, # optional name of operation to run
     schema    => sub {}, # Graph::QL::Schema
     query     => sub {}, # Graph::QL::Operation::Query
-    resolvers => sub { +{} }, # a mapping of TypeName to Resolver instance
+    resolvers => sub {}, # a mapping of TypeName to Resolver instance
     # internals ...
-    _data     => sub { +{} },
     _errors   => sub { +[] },
 );
 
 sub BUILDARGS : strict(
-    schema     => schema,
-    query      => query,
-    resolvers? => resolvers,
+    schema    => schema,
+    query     => query,
+    resolvers => resolvers,
 );
 
 sub BUILD ($self, $params) {
@@ -44,15 +42,11 @@ sub BUILD ($self, $params) {
     # - handle `variables`
     # - handle `context-value`
 
-    if ( exists $params->{resolvers} ) {
-        throw('The `resolvers` must be a HASH ref, not `%s`', $self->{resolvers})
-            unless assert_non_empty( $self->{resolvers} );
+    throw('The `resolvers` must be a HASH ref, not `%s`', $self->{resolvers})
+        unless assert_hashref( $self->{resolvers} );
 
-        foreach ( values $self->{resolvers}->%* ) {
-             throw('The values in `resolvers` must all be of type(Graph::QL::Execution::FieldResolver), not `%s`', $_ )
-                unless assert_isa( $_, 'Graph::QL::Execution::FieldResolver' );
-        }
-    }
+    throw('The `resolvers` HASH can not be empty')
+        unless assert_non_empty( $self->{resolvers} );
 }
 
 sub schema : ro;
@@ -61,8 +55,9 @@ sub query  : ro;
 sub has_errors ($self) { !! scalar $self->{_errors}->@* }
 sub get_errors ($self) {           $self->{_errors}->@* }
 
-sub has_data ($self) { !! scalar keys $self->{_data}->%* }
-sub get_data ($self) {                $self->{_data}->%* }
+sub get_resolver_for_type ($self, $type) {
+    return $self->{resolvers}->{ $type };
+}
 
 ## ...
 
@@ -75,7 +70,7 @@ sub validate ($self) {
     );
 
     # validate the schema ...
-    $v->validate( $self->{name} );
+    $v->validate;
 
     # if the validation succeeds,
     # there are no errors ...
@@ -89,22 +84,95 @@ sub validate ($self) {
 
 sub execute ($self) {
 
-    throw('You cannot execute a query that has errors')
+    throw("You cannot execute a query that has errors:\n%s" => join "\n" => $self->get_errors)
         if $self->has_errors;
 
+    my $root_type = $self->{schema}->lookup_root_type( $self->{query} );
 
+    my $data = $self->execute_selections(
+        $root_type,
+        $self->{query}->selections,
+        $self->get_resolver_for_type( $root_type->name ),
+        {},
+    );
+
+    return $data;
+}
+
+sub execute_selections ($self, $schema_type, $selections, $resolvers, $initial_value) {
+
+    my %results;
+    foreach my $selection ( $selections->@* ) {
+        my $response_key = $selection->has_alias ? $selection->alias : $selection->name;
+        my $schema_field = $schema_type->lookup_field( $selection );
+
+        $results{ $response_key } = $self->execute_field(
+            $schema_field,
+            $selection,
+            $resolvers->{ $schema_field->name },
+            $initial_value
+        );
+    }
+
+    return \%results;
+}
+
+sub execute_field ($self, $schema_field, $selection, $resolver, $initial_value) {
+
+    my %field_args = map { $_->name => $_->value } $selection->args->@*;
+    my $resolved   = $resolver->( $initial_value, \%field_args );
+
+    my $schema_field_type = $self->{schema}->lookup_type(
+        $self->_find_base_schema_type( $schema_field->type )
+    );
+
+    if ( $selection->has_selections ) {
+
+        my $selections = $selection->selections;
+        my $resolvers  = $self->get_resolver_for_type( $schema_field_type->name );
+
+        if ( $schema_field->type->isa('Graph::QL::Schema::Type::Named') ) {
+            $resolved = $self->execute_selections(
+                $schema_field_type,
+                $selections,
+                $resolvers,
+                $resolved,
+            );
+        }
+        else {
+            if ( $schema_field->type->isa('Graph::QL::Schema::Type::List') ) {
+                $resolved = [
+                    map {
+                        $self->execute_selections(
+                            $schema_field_type,
+                            $selections,
+                            $resolvers,
+                            $_,
+                        )
+                    } $resolved->@*
+                ]
+            }
+            elsif ( $schema_field->type->isa('Graph::QL::Schema::Type::NonNull') ) {
+
+            }
+        }
+    }
+
+    return $resolved;
 }
 
 ## ...
 
+sub _find_base_schema_type ($self, $schema_type) {
+    $schema_type = $schema_type->of_type
+        while $schema_type->isa('Graph::QL::Schema::Type::NonNull')
+           || $schema_type->isa('Graph::QL::Schema::Type::List');
+    return $schema_type;
+}
+
 sub _add_error ($self, $msg, @args) {
     $msg = sprintf $msg => @args if @args;
     push $self->{_errors}->@* => $msg;
-    return;
-}
-
-sub _add_data_key ($self, $key, $value) {
-    $self->{_data}->{ $key } = $value;
     return;
 }
 
