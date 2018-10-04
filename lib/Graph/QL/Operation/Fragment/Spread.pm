6fa48b495b9d81f16db66df3a40a29be67543961
Stevan Little <stevan.little@gmail.com>
2018-10-04 23:34:20 +0200

    stub out fragments

---
+++ lib/Graph/QL/Operation/Fragment/Spread.pm
@@ -0,0 +1,66 @@
+package Graph::QL::Operation::Fragment::Spread;
+# ABSTRACT: GraphQL in Perl
+use v5.24;
+use warnings;
+use experimental 'signatures', 'postderef';
+use decorators ':accessors', ':constructor';
+
+use Graph::QL::Util::Errors     'throw';
+use Graph::QL::Util::Assertions 'assert_isa', 'assert_non_empty';
+
+use Graph::QL::AST::Node::FragmentSpread;
+use Graph::QL::AST::Node::Name;
+
+our $VERSION = '0.01';
+
+use parent 'UNIVERSAL::Object::Immutable';
+use slots (
+    _ast        => sub {},
+    _name       => sub {},
+);
+
+sub BUILDARGS : strict(
+    ast?        => _ast,
+    name?       => _name,
+);
+
+sub BUILD ($self, $params) {
+
+    if ( exists $params->{_ast} ) {
+        throw('The `ast` must be an instance of `Graph::QL::AST::Node::FragmentSpread`, not `%s`', $self->{_ast})
+            unless assert_isa( $self->{_ast}, 'Graph::QL::AST::Node::FragmentSpread' );
+
+        $self->{_name} = $self->{_ast}->name->value;
+
+        # ...
+    }
+    else {
+
+        throw('You must pass a defined value to `name`')
+            unless defined $self->{_name};
+
+        $self->{_ast} = Graph::QL::AST::Node::FragmentSpread->new(
+            name => Graph::QL::AST::Node::Name->new( value => $self->{_name} ),
+            # ...
+        );
+    }
+}
+
+sub ast  : ro(_);
+sub name : ro(_);
+
+## ...
+
+sub to_type_language ($self) {
+
+}
+
+
+1;
+
+__END__
+
+=pod
+
+=cut
+
