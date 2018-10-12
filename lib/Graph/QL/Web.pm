fd09ebc94a1481dc4e3ec8b906cae17f3ff24153
Stevan Little <stevan.little@gmail.com>
2018-10-12 07:30:58 +0200

    stub this out

---
+++ lib/Graph/QL/Web.pm
@@ -0,0 +1,15 @@
+package Graph::QL::Web;
+# ABSTRACT: GraphQL in Perl
+use v5.24;
+use warnings;
+use experimental 'signatures', 'postderef';
+
+our $VERSION = '0.01';
+
+1;
+
+__END__
+
+=pod
+
+=cut
